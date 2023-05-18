ff76f7e631278368dc3c33022fac89420306d92d
AZ9tumas <apc0948@gmail.com>
2023-05-18 10:11:09 +0400

    Added a few instructions in cpu.c

--- cpu.c
+++ cpu.c
@@ -1,12 +1,29 @@
 #include "cpu.h"
 
-#define INC(emu, reg, byte) \
-    inc_r8(emu, emu->reg.bytes.byte); \
-    emu->reg.bytes.byte += 1; \
+/* Register definations */
 
-#define DEC(emu, reg, byte) \
-    dec_r8(emu, emu->reg.bytes.byte); \
-    emu->reg.bytes.byte -= 1; \
+#define A(emu) emu->AF.bytes.higher
+#define F(emu) emu->AF.bytes.lower
+
+#define B(emu) emu->BC.bytes.higher
+#define C(emu) emu->BC.bytes.lower
+
+#define D(emu) emu->DE.bytes.higher
+#define E(emu) emu->DE.bytes.lower
+
+#define H(emu) emu->HL.bytes.higher
+#define L(emu) emu->HL.bytes.lower
+
+#define AF(emu) emu->AF.entireByte
+#define BC(emu) emu->BC.entireByte
+#define DE(emu) emu->DE.entireByte
+#define HL(emu) emu->HL.entireByte
+
+#define LD_u16(emu, reg) emu->reg.entireByte = read_u16(emu);
+#define LD_addr_reg(emu, addr, reg) write(emu, addr,  reg);
+
+#define INC(emu, reg) inc_r8(emu, reg); reg ++;
+#define DEC(emu, reg) dec_r8(emu, reg); reg --;
 
 /* Some very imp functions */
 
@@ -23,13 +40,18 @@ u8 read(Emulator* emu, u16 addr){
     return 0xff;
 }
 
+u16 read_u16(Emulator* emu){
+    return (u16)(read(emu, emu->PC.entireByte++) | (read(emu, emu->PC.entireByte++) << 8));
+}
+
 void write(Emulator* emu, u16 addr, u8 byte){
     if ((addr >= ECHO_RAM && addr <= ECHO_RAM_END) || (addr >= NOT_USABLE && addr <= NOT_USABLE_END)) return;
+    
     if (addr >= VRAM_8KB && addr <= VRAM_8KB_END) emu->vram[addr - VRAM_8KB] = byte;
-    if (addr >= WRAM_4KB && addr <= WRAM_4KB_END) emu->wram1[addr - WRAM_4KB] = byte;
-    if (addr >= WRAM_SWITCHABLE_4KB && addr <= WRAM_SWITCHABLE_4KB_END) emu->wram2[addr - WRAM_SWITCHABLE_4KB] = byte;
-    if (addr >= HIGH_RAM && addr <= HIGH_RAM_END) emu->hram[addr - HIGH_RAM] = byte;
-    if (addr >= IO_REGISTERS && addr <= IO_REGISTERS_END) emu->IO[addr - IO_REGISTERS] = byte;
+    else if (addr >= WRAM_4KB && addr <= WRAM_4KB_END) emu->wram1[addr - WRAM_4KB] = byte;
+    else if (addr >= WRAM_SWITCHABLE_4KB && addr <= WRAM_SWITCHABLE_4KB_END) emu->wram2[addr - WRAM_SWITCHABLE_4KB] = byte;
+    else if (addr >= HIGH_RAM && addr <= HIGH_RAM_END) emu->hram[addr - HIGH_RAM] = byte;
+    else if (addr >= IO_REGISTERS && addr <= IO_REGISTERS_END) emu->IO[addr - IO_REGISTERS] = byte;
 }
 
 void Start(Cartridge* cart, Emulator* emu){
@@ -64,8 +86,10 @@ void dispatch(Emulator* emu){
 
     switch (opcode){
         case 0x00: break;
-        case 0x04: INC(emu, BC, higher); break;
-        case 0x05: DEC(emu, BC, higher); break;
+        case 0x01: LD_u16(emu, BC);
+        case 0x02: LD_addr_reg(emu, read(emu, BC(emu)), A(emu));
+        case 0x04: INC(emu, B(emu)); break;
+        case 0x05: DEC(emu, B(emu)); break;
         
     }
 
